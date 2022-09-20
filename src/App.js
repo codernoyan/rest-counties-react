dbf0cc485072d4af534d18444db1c2c36cf417fd
codernoyan <coder.noyan@gmail.com>
2022-09-20 11:02:37 +0600

    rest counties data fetched with useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countries></Countries>
     </div>
   );
+}
+
+function Countries(props) {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+      .catch(error => console.log(error));
+  }, [])
+  return (
+    <div>
+      <h2><code>Countries</code></h2>
+      {
+        countries.map(country => <Country name={country.name.common} capital={country.capital} flag={country.flags.png}></Country>)
+      }
+    </div>
+  )
+}
+
+const Country = props => {
+  return (
+    <div className="country">
+      <div style={{ width: "100%"}}>
+      <h2>Country Name: {props.name}</h2>
+      <h4>Capital: {props.capital}</h4>
+      </div>
+      <div style={{width: "100%"}}>
+      <img src={props.flag} alt="flag" />
+      </div>
+    </div>
+  )
 }
 
 export default App;
